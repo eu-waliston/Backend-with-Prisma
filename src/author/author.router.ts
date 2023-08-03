aa4cd7f38e0662cc1483f73b29a58afabcc8dc51
Waliston <goneisgone@hotmail.com>
2023-08-03 08:48:16 -0300

    RestfullAPI with TS, Author Services and Router

--- src/author/author.router.ts
+++ src/author/author.router.ts
@@ -11,7 +11,82 @@ authorRouter.get("/", async (request: Request, response: Response) => {
   try {
     const authors = await AuthorService.listAuthors();
     return response.status(200).json(authors);
-  } catch (error) {
+  } catch (error: any) {
     return response.status(500).json(error);
   }
 });
+
+//GET a single author ID
+authorRouter.get("/:id", async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+
+  try {
+    const author = await AuthorService.getAuthor(id);
+    if (author) {
+      return response.status(200).json(author);
+    }
+    return response.status(404).json("Author could nopt be found");
+  } catch (error: any) {
+    return response.status(500).json(error);
+  }
+});
+
+//POST: create a aauthor
+//Params: Fname & Lname
+authorRouter.post(
+  "/",
+  body("firstName").isString(),
+  body("lastName").isString(),
+  async (request: Request, response: Response) => {
+    const erros = validationResult(request);
+    if (!erros.isEmpty()) {
+      return response.status(400).json({ erros: erros.array() });
+    }
+
+    try {
+      const author = request.body;
+      const newAuthor = await AuthorService.createAuthor(author);
+      return response.status(201).json(newAuthor);
+    } catch (error: any) {
+      return response.status(500).json(error);
+    }
+  }
+);
+
+//PUT: updating an aauthor
+//Params: Fname & Lname
+authorRouter.put(
+  "/:id",
+  body("firstName").isString(),
+  body("lastName").isString(),
+  async (request: Request, response: Response) => {
+    const erros = validationResult(request);
+    if (!erros.isEmpty()) {
+      return response.status(400).json({ erros: erros.array() });
+    }
+
+    const id: number = parseInt(request.params.id, 10);
+    try {
+      const author = request.body;
+      const updateAuthor = await AuthorService.updateAuthor(author, id);
+      return response.status(200).json(updateAuthor);
+    } catch (error: any) {
+      return response.status(500).json(error);
+    }
+  }
+);
+
+
+//DELETE: delete an aauthor based on the ID
+authorRouter.delete("/:id", async(request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+
+  try {
+    await AuthorService.deleteAuthor(id);
+    return response.status(204).send("Author has been successfully delected!")
+  } catch (error: any) {
+    return response.status(500).json(error);
+    
+  }
+
+})
--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -23,5 +23,56 @@ export const getAuthor = async (id: number): Promise<Author | null> => {
     where: {
       id,
     },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+    },
+  });
+};
+
+export const createAuthor = async (
+  author: Omit<Author, "id">
+): Promise<Author> => {
+  const { firstName, lastName } = author;
+  return db.author.create({
+    data: {
+      firstName,
+      lastName,
+    },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+    },
+  });
+};
+
+export const updateAuthor = async (
+  author: Omit<Author, "id">,
+  id: number
+): Promise<Author> => {
+  const { firstName, lastName } = author;
+  return db.author.update({
+    where: {
+      id,
+    },
+    data: {
+      firstName,
+      lastName,
+    },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+    },
+  });
+};
+
+export const deleteAuthor = async (id: number): Promise<void> => {
+  await db.author.delete({
+    where: {
+      id,
+    },
   });
 };
